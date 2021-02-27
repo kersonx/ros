a7cb4efb54142816fcf6e7e143c84d10adeae75f
kersonx <youneskaouan@gmail.com>
2021-02-27 15:37:10 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,18 +10,14 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-
-sudo apt install lightdm
-sudo apt install tasksel
-sudo apt tasksel install kde-plasma-desktop
-
+apt install --assume-yes xfce4 desktop-base
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo apt install nautilus nano -y 
-sh <(wget -qO - https://downloads.nordcdn.com/apps/linux/install.sh)
+sudo apt -y install firefox
 
 
 
