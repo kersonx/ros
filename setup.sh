734ca054e648cc877d1456fd80f7c9774bd076c9
kersonx <youneskaouan@gmail.com>
2021-04-29 23:27:25 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,4 +26,9 @@ printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debia
 read -p "Paste Here: " CRP
 su - chungvh -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
+if sudo apt-get upgrade &> /dev/null
+then
+    printf "\n\nUpgrade Completed " >&2
+else
+    printf "\n\nError Occured " >&2
 fi
