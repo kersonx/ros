267e627c879ffca30eb7abeedfca6c4d83b2b38f
kersonx <youneskaouan@gmail.com>
2021-04-30 12:37:57 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,38 +1,40 @@
+#! /usr/bin/env python
+with open ('script.sh', 'w') as f:
+f.write('''\
 #! /bin/bash
 printf "Installing RDP Be Patience... " >&2
 {
-sudo useradd -m chungvh
-sudo adduser chungvh sudo
-echo 'chungvh:your_passwd' | sudo chpasswd
+sudo useradd -m phcorner
+sudo adduser phcorner sudo
+echo 'phcorner:0000' | sudo chpasswd
 sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
-mkdir -p ~/.config/chrome-remote-desktop
-chown "$USER:$USER" ~/.config/chrome-remote-desktop
-chmod a+rx ~/.config/chrome-remote-desktop
 apt install --assume-yes xfce4 desktop-base
-sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-
-sudo apt install nautilus nano -y 
-sudo adduser chungvh chrome-remote-desktop
+sudo apt install nautilus nano -y
+sudo adduser phcorner chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Complete " >&2 ||
 printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+printf '\nCheck https://remotedesktop.google.com/headless Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - chungvh -c """$CRP"""
+su - phcorner -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 if sudo apt-get upgrade &> /dev/null
 then
-    printf "\n\nUpgrade Completed " >&2
+printf "\n\nUpgrade Completed " >&2
 else
-    printf "\n\nError Occured " >&2
+printf "\n\nError Occured " >&2
 fi
+''')
+! chmod +x script.sh
+! ./script.sh
